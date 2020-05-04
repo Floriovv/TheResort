68f9feb0f763d678345513389b924ad8af0bf40e
Florian Bollmann <florian@gentlent.com>
2020-05-04 19:33:13 +0200

    JS Flags

---
+++ assets/js/main.js
@@ -0,0 +1,21 @@
+document.getElementById("choose-b").addEventListener("mouseover", function(){
+  document.getElementById("choose-b").style.opacity = 1;
+  document.getElementById("choose-c").style.opacity = .5;
+  document.getElementById("choose-a").style.opacity = .5;
+})
+
+document.getElementById("choose-c").addEventListener("mouseover", function(){
+  document.getElementById("choose-b").style.opacity = .5;
+  document.getElementById("choose-c").style.opacity = 1;
+  document.getElementById("choose-a").style.opacity = .5;
+})
+
+document.getElementById("choose-a").addEventListener("mouseover", function(){
+  document.getElementById("choose-b").style.opacity = .5;
+  document.getElementById("choose-c").style.opacity = .5;
+  document.getElementById("choose-a").style.opacity = 1;
+})
+
+document.getElementById("choose-b").addEventListener("click", function(){
+  document.getElementById("choose-text-b").style.opacity = 1;
+})
