d3f8839b25af5310525ef38c2cac83d5f0a0e558
Florian Bollmann <florian@gentlent.com>
2020-05-05 15:24:34 +0200

    Faggen und Text ein und ausblendbar + Zurückbutton

--- assets/js/main.js
+++ assets/js/main.js
@@ -27,12 +27,40 @@ document.getElementById("choose-a").addEventListener("mouseover", function(){
 
 document.getElementById("choose-b").addEventListener("click", function(){
   document.getElementById("choose-text-b").style.opacity = 1;
+  document.getElementById("choose-c").style.display = "none";
+  document.getElementById("choose-a").style.display = "none";
+  document.getElementById("CsatSoldat").style.display = "none";
+  document.getElementById("IndependentSoldat").style.display = "none";
+  document.getElementById("backbutton").style.display = "inline-block";
 })
 
 document.getElementById("choose-c").addEventListener("click", function(){
   document.getElementById("choose-text-c").style.opacity = 1;
+  document.getElementById("choose-b").style.display = "none";
+  document.getElementById("choose-a").style.display = "none";
+  document.getElementById("NatoSoldat").style.display = "none";
+  document.getElementById("IndependentSoldat").style.display = "none";
+  document.getElementById("backbutton").style.display = "inline-block";
 })
 
 document.getElementById("choose-a").addEventListener("click", function(){
   document.getElementById("choose-text-a").style.opacity = 1;
+  document.getElementById("choose-c").style.display = "none";
+  document.getElementById("choose-b").style.display = "none";
+  document.getElementById("CsatSoldat").style.display = "none";
+  document.getElementById("NatoSoldat").style.display = "none";
+  document.getElementById("backbutton").style.display = "inline-block";
 })
+
+function backbutton(){
+  document.getElementById("choose-c").style.display = "unset";
+  document.getElementById("choose-b").style.display = "unset";
+  document.getElementById("choose-a").style.display = "unset";
+  document.getElementById("CsatSoldat").style.display = "unset";
+  document.getElementById("NatoSoldat").style.display = "unset";
+  document.getElementById("IndependentSoldat").style.display = "unset";
+  document.getElementById("choose-text-a").style.opacity = 0;
+  document.getElementById("choose-text-c").style.opacity = 0;
+  document.getElementById("choose-text-b").style.opacity = 0;
+  document.getElementById("backbutton").style.display = "none";
+}
